a2c59fcaefb2b4a09b70ffda74991047ccfbcff3
olgafurs <olga.m.furs@mail.ru>
2020-10-05 13:02:51 +0300

    init: add resolved of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reverseNum = [];
+
+    if (n < 0) {
+        n = -1 * n;
+    }
+
+    for (let i = 0; i < String(n).length; i++) {
+        reverseNum.push(String(n)[i]);
+    }
+    
+    reverseNum = Number(reverseNum.reverse().join(''));
+
+    return reverseNum;
 }
